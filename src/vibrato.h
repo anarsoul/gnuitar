bd8e5c224c6614d1f4dc2498ae51fae3a67a160d
Max V. Rudensky <fonin@omnistaronline.com>
2003-02-01 19:14:21 +0000

    New constant VIBRATO_THRESHOLD moved from vibrato.c

--- src/vibrato.h
+++ src/vibrato.h
@@ -25,13 +25,14 @@
 
 #include "pump.h"
 
-#define MAX_VIBRATO_BUFSIZE 20000
+#define MAX_VIBRATO_BUFSIZE SAMPLE_RATE
+#define VIBRATO_THRESHOLD 1500
 
 extern void     vibrato_create(struct effect *);
 
 struct vibrato_params {
-    int             vibrato_amplitude,
-                    vibrato_speed,
+    float           vibrato_amplitude;
+    int             vibrato_speed,
                     vibrato_phase_buffer_size;
     int             vibrato_phase,
                    *phase_buffer;
