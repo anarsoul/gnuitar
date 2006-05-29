5691235e18155df220505bee8a3bfb9ed985bb86
Vasily Khoruzhick <anarsoul@gmail.com>
2006-05-29 18:35:39 +0000

    Added macroses id and log to files

--- src/amp.c
+++ src/amp.c
@@ -1,6 +1,28 @@
 /*
  * GNUitar
  * Amp effect
+ * Copyright (C) 2006 Vasily Khoruzhick         <anarsoul@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id$
+ *
+ * $Log$
+ * Revision 1.3  2006/05/29 18:35:39  anarsoul
+ * Added macroses id and log to files
+ *
  */
 
 #include "amp.h"
